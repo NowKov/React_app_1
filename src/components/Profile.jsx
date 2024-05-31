e5d837f9eba56bb7d75a49439b0d1c912f9d0bd8
NowKov <genesis-45@yandex.ru>
2024-05-31 17:31:35 +0300

    3

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,10 @@ import "./../App.css"
 const Profile = () =>{
     return(
         <div className="content">
-            
+            <div className="image_length">
+                <img src="https://img.goodfon.ru/original/3360x1050/b/62/usa-new_york-nyc-city-gorod-5663.jpg"></img>
+                Contents
+            </div>
         </div>
     );
 
