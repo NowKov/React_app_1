7ade152217b0bb1315201de290eabaaab14dc1bc
NowKov <genesis-45@yandex.ru>
2024-06-11 20:25:30 +0300

    End_Navbar_Active

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,37 @@
-import { NavLink } from "react-router-dom";
-import class_navbar from "./Navbar.module.css";
+import { Link, NavLink } from "react-router-dom";
+// import class_navbar from "./Navbar.module.css";
+import class_navbar from "./Navbar1.module.css";
+// import "./Navbar.css";
+const setActive = ({ isActive }) =>
+  isActive ? class_navbar.activeLink : class_navbar.disactiveLink;
 
 const Navbar = () => {
   return (
     <nav className={class_navbar.nav}>
-      <div className={`${class_navbar.item} ${class_navbar.news}`}>
-        <NavLink to="/dialogs">Messages</NavLink>
+      <div className={class_navbar.item}>
+        <NavLink to="/dialogs" className={setActive}>
+          Messages
+        </NavLink>
       </div>
-      <div className={`${class_navbar.item} ${class_navbar.feed}`}>
-        <NavLink to="/profile">Profile</NavLink>
+      <div className={class_navbar.item}>
+        <NavLink to="/profile" className={setActive}>
+          Profile
+        </NavLink>
       </div>
-      <div className={`${class_navbar.item} ${class_navbar.settings}`}>
-        <NavLink to="/news">News</NavLink>
+      <div className={class_navbar.item}>
+        <NavLink to="/news" className={setActive}>
+          News
+        </NavLink>
       </div>
-      <div className={`${class_navbar.item} ${class_navbar.music}`}>
-        <NavLink to="/music">Music</NavLink>
+      <div className={class_navbar.item}>
+        <NavLink to="/music" className={setActive}>
+          Music
+        </NavLink>
       </div>
-      <div className={`${class_navbar.item} ${class_navbar.music}`}>
-        <NavLink to="/settings">Settings</NavLink>
+      <div className={class_navbar.item}>
+        <NavLink to="/settings" className={setActive}>
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
